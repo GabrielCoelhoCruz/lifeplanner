616ff60f7fb38451940bc8dc1a24a0822f113188
GabrielCoelhoCruz <43425553+GabrielCoelhoCruz@users.noreply.github.com>
2026-04-16 09:37:01 -0300

    chore: add .output to gitignore

--- .output/server/_ssr/projects._projectId-DRXsSlnu.mjs
+++
@@ -1,9 +0,0 @@
-import { c as createFileRoute, s as lazyRouteComponent } from "../_libs/@tanstack/react-router+[...].mjs";
-//#region node_modules/.nitro/vite/services/ssr/assets/projects._projectId-DRXsSlnu.js
-var $$splitComponentImporter = () => import("./projects._projectId-Ydy_LpbC.mjs");
-var Route = createFileRoute("/projects/$projectId")({
-	component: lazyRouteComponent($$splitComponentImporter, "component"),
-	validateSearch: (search) => ({ view: search.view === "kanban" ? "kanban" : void 0 })
-});
-//#endregion
-export { Route as t };
